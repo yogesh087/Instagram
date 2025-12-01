90b2adc4fae424e25d013e2f21cbc6f081278a5b
yogesh087 <yogeshkanojiya7771@gmail.com>
2025-12-01 19:17:24 +0530

    do some chnages

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
+require("dotenv").config();
 const express = require('express');
-const app = express()
-const port = process.env.port || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const { mongoUrl } = require("./keys");
 const cors = require("cors");
-const path = require("path")
+const path = require("path");
 
-app.use(cors())
-require('./models/model')
-require('./models/post')
-app.use(express.json())
-app.use(require("./routes/auth"))
-app.use(require("./routes/createPost"))
-app.use(require("./routes/user"))
+app.use(cors());
+require('./models/model');
+require('./models/post');
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/createPost"));
+app.use(require("./routes/user"));
+
 mongoose.connect(mongoUrl);
 
 mongoose.connection.on("connected", () => {
-    console.log("successfully connected to mongo")
-})
+    console.log("successfully connected to mongo");
+});
 
 mongoose.connection.on("error", () => {
-    console.log("not connected to mongodb")
-})
+    console.log("not connected to mongodb");
+});
 
 // serving the frontend
-app.use(express.static(path.join(__dirname, "./frontend/build")))
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", (req, res) => {
     res.sendFile(
         path.join(__dirname, "./frontend/build/index.html"),
         function (err) {
-            res.status(500).send(err)
+            res.status(500).send(err);
         }
-    )
-})
-
-
+    );
+});
 
 app.listen(port, () => {
-    console.log("server is running on port" + " " + port)
-
-})
+    console.log("server is running on port " + port);
+});
